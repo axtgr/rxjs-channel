dd8e6a5ab88a1a85f28e37c711591ead8e9705f6
Alex Shnayder <alex@shnayder.pro>
2017-03-21 21:20:29 +0700

    Support Channel lifting

--- src/Channel.js
+++ src/Channel.js
@@ -4,88 +4,131 @@ const ObservablePool = require('./ObservablePool');
 
 
 module.exports =
-class Channel extends Subject {
-  constructor() {
-    super();
-    this.pool = new ObservablePool(Rendezvous);
-  }
-
-  _findMatch(op) {
-    let match = this.pool
-      .filter(r => r.put === op || r.consume === op)
-      .zip(op, r => r.value)
-      .takeLast(1);
-    let result = new ReplaySubject(1);
-    match.subscribe(result);
-    return result;
-  }
-
-  consume() {
-    if (this.hasError) {
-      return Observable.throw(this.thrownError);
-    } else if (this.isStopped) {
-      return Observable.empty();
+  class Channel extends Subject {
+    constructor() {
+      super();
+      this.pool = new ObservablePool(Rendezvous);
     }
 
-    let op = {};
-    let rendezvous = this.pool.get(r => !r.consume);
-    rendezvous.setOp('consume', op);
-    return rendezvous;
-  }
+    lift(operator) {
+      let instance = new this.constructor();
 
-  put(value) {
-    if (this.hasError) {
-      return Observable.throw(this.thrownError);
-    } else if (this.isStopped) {
-      return Observable.empty();
+      if (operator) {
+        instance.operator = operator;
+      }
+
+      instance.source = this;
+      return instance;
     }
 
-    let op = {};
-    let rendezvous = this.pool.get(r => !r.put);
-    rendezvous.setOp('put', op);
-    rendezvous.setValue(value);
-    super.next(value);
-    return rendezvous;
-  }
-
-  next(value) {
-    this.put(value);
-  }
-
-  add(value) {
-    if (this.hasError) {
-      return Observable.throw(this.thrownError);
-    } else if (this.isStopped) {
-      return Observable.empty();
+    _findMatch(op) {
+      let match = this.pool
+        .filter(r => r.put === op || r.consume === op)
+        .zip(op, r => r.value)
+        .takeLast(1);
+      let result = new ReplaySubject(1);
+      match.subscribe(result);
+      return result;
     }
 
-    let op;
+    consume() {
+      if (this.source && this.source.consume) {
+        return this.source.consume();
+      }
 
-    if (value && typeof value.subscribe === 'function') {
-      op = value;
-    } else if (value && typeof value.then === 'function') {
-      op = Observable.from(value);
-    } else {
-      return this.put(value);
+      if (this.hasError) {
+        return Observable.throw(this.thrownError);
+      } else if (this.isStopped) {
+        return Observable.empty();
+      }
+
+      let op = {};
+      let rendezvous = this.pool.get(r => !r.consume);
+      rendezvous.setOp('consume', op);
+      return rendezvous;
     }
 
-    op.subscribe(value => {
+    put(value) {
+      if (this.source && this.source.put) {
+        return this.source.put(value);
+      }
+
+      if (this.hasError) {
+        return Observable.throw(this.thrownError);
+      } else if (this.isStopped) {
+        return Observable.empty();
+      }
+
+      let op = {};
       let rendezvous = this.pool.get(r => !r.put);
       rendezvous.setOp('put', op);
       rendezvous.setValue(value);
       super.next(value);
-    });
+      return rendezvous;
+    }
 
-    return this._findMatch(op);
-  }
+    next(value) {
+      if (this.source && this.source.next) {
+        return this.source.next(value);
+      }
 
-  complete() {
-    super.complete();
-    this.pool.complete();
-  }
+      this.put(value);
+    }
 
-  error(err) {
-    super.error(err);
-    this.pool.error(err);
-  }
-};
+    add(value) {
+      if (this.source && this.source.add) {
+        return this.source.add(value);
+      }
+
+      if (this.hasError) {
+        return Observable.throw(this.thrownError);
+      } else if (this.isStopped) {
+        return Observable.empty();
+      }
+
+      let op;
+
+      if (value && typeof value.subscribe === 'function') {
+        op = value;
+      } else if (value && typeof value.then === 'function') {
+        op = Observable.from(value);
+      } else {
+        return this.put(value);
+      }
+
+      op.subscribe(value => {
+        let rendezvous = this.pool.get(r => !r.put);
+        rendezvous.setOp('put', op);
+        rendezvous.setValue(value);
+        super.next(value);
+      });
+
+      return this._findMatch(op);
+    }
+
+    complete() {
+      if (this.source && this.source.complete) {
+        return this.source.complete();
+      }
+
+      super.complete();
+      this.pool.complete();
+    }
+
+    error(err) {
+      if (this.source && this.source.error) {
+        return this.source.error(err);
+      }
+
+      super.error(err);
+      this.pool.error(err);
+    }
+
+    _subscribe(subscriber) {
+      if (this.source) {
+        return this.source.subscribe(subscriber);
+      }
+
+      return super._subscribe(subscriber);
+    }
+  };
